81599f0a60490c959fc1bcd86e1edd97410f4678
catwent <37201659+catwent@users.noreply.github.com>
2020-03-31 19:24:41 -0400

    added CMS exercise

---
+++ exercises/0_arena/index.js
@@ -0,0 +1,61 @@
+let channel = '8x8-images';
+let makeURL = (per, page) => `https://api.are.na/v2/channels/${channel}?per=${per}&page=${page}`;
+
+// Get metadata
+fetch(makeURL(1, 1))
+  .then((res) => res.json())
+  .then((json) => {
+    document.querySelector('#title').innerHTML = json.title;
+  });
+
+// Get the blocks
+fetch(makeURL(1, 1))
+  .then((res) => res.json())
+  .then((json) => count = json.length)
+  .then((count) => {
+    let per = 100;
+    let pages = Math.ceil(count/per);
+
+    let fetches = [];
+    for (let page = 1; page <= pages; page++) {
+      fetches.push(fetch(makeURL(per, page)).then((res) => res.json()).then((json) => json.contents));
+    }
+
+    Promise.all(fetches)
+      .then((contents) => {
+        contents.forEach((content) => {
+          content.forEach((entry) => {
+            makeEntry(entry);
+        });
+      });
+    });
+  });
+
+
+function makeEntry(entry) {
+  let entryTemplate = document.getElementById("entry-template");
+  let entryEl = entryTemplate.content.cloneNode(true);
+  let entryLi = entryEl.querySelector('li');
+
+  let entryClass = entry.class;
+
+  // if (entry.class === 'Text') {
+  //   let text = entry.content_html;
+  //   entryLi.innerHTML = text;
+  // } else if (entry.class === 'Image') {
+  //   entryLi.innerHTML = '<a href="' + entry.image.original.url + '" target="_blank"><img src="' + entry.image.display.url + '"></a><div class="caption">' + entry.title + '</div>';
+  // }
+
+if (entryClass == 'Image') {
+entryLi.querySelector('a').href = entry.image.original.ur;
+entryLi.querySelector('img').src = entry.image.display.url;
+entryLi.querySelector('.title').innerHTML = entry.title;
+entryLi.querySelector('.title').innerHTML = entry.title;
+entryLi.querySelector('.description').innerHTML = entry.description_html;
+} else if (entryClass == 'Text') { 
+    entryLi.querySelector('.description').innerHTML = entry.content_html; 
+} 
+
+  let entriesEl = document.getElementById('entries');
+  entriesEl.insertBefore(entryEl, entriesEl.firstChild);
+}
